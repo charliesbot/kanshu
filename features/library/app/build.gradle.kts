ea40c8fa5b2d9d988ac002ba8336de44518a2c2e
Charlie L <charliesbot@gmail.com>
2026-05-17 17:13:31 -0700

    use alpha compose to have access to grid component

--- features/library/app/build.gradle.kts
+++ features/library/app/build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
   implementation(platform(libs.koin.bom))
   implementation(libs.androidx.compose.ui)
   implementation(libs.androidx.compose.foundation)
+  implementation(libs.androidx.compose.foundation.layout)
   implementation(libs.composeunstyled)
   implementation(libs.androidx.compose.ui.tooling.preview)
   implementation(libs.androidx.lifecycle.runtime.ktx)
