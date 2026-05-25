278824219614e46770af6cecd375b5cbf89d1516
Charlie <charliesbot@gmail.com>
2026-05-24 22:06:54 -0700

    [refactor] Reset reader renderer to WebView shell (#33)

--- features/reader/app/src/main/kotlin/com/charliesbot/kanshu/features/reader/di/ReaderModule.kt
+++ features/reader/app/src/main/kotlin/com/charliesbot/kanshu/features/reader/di/ReaderModule.kt
@@ -1,9 +1,5 @@
 package com.charliesbot.kanshu.features.reader.di
 
-import com.charliesbot.kanshu.features.reader.ReaderViewModel
-import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
-val readerModule = module {
-  viewModel { (seriesId: Int) -> ReaderViewModel(seriesId, get(), get(), get()) }
-}
+val readerModule = module {}
