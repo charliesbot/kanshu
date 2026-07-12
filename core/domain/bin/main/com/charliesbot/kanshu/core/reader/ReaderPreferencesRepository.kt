949190d8f04781ffea38d0ff06dd83edb014eafd
Charlie L <charliesbot@gmail.com>
2026-07-11 17:44:03 -0700

    upgrade dependencies

--- core/domain/bin/main/com/charliesbot/kanshu/core/reader/ReaderPreferencesRepository.kt
+++ core/domain/bin/main/com/charliesbot/kanshu/core/reader/ReaderPreferencesRepository.kt
@@ -8,4 +8,18 @@ interface ReaderPreferencesRepository {
   suspend fun setFont(font: ReaderFont)
 
   suspend fun setFontScale(scale: Float)
+
+  suspend fun setMargins(margins: ReaderMargins)
+
+  suspend fun setAlignment(alignment: ReaderAlignment)
+
+  suspend fun setLineSpacing(value: Float)
+
+  suspend fun setParagraphSpacing(value: Float)
+
+  suspend fun setWordSpacing(value: Float)
+
+  suspend fun setLetterSpacing(value: Float)
+
+  suspend fun resetSpacing()
 }
